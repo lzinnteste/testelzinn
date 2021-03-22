c86b1492ddb35dbe5cf0155f1f4e762f8e0cd0f3
lzinnteste <bete.fernandes02@gmail.com>
2021-03-22 18:32:47 -0300

    First commit

---
+++ settings/config.js
@@ -0,0 +1,4 @@
+const config = {
+        botName: 'LzinnBOT',
+        ownerName: Lzinn 
+}
---
+++ src/toinmenu.js
@@ -0,0 +1,12 @@
+const toinmenu = (prefix, pushname) => {
+    return `◪ *Comandos do LzinnBot*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.Lzinnmenu = Lzinnmenu
